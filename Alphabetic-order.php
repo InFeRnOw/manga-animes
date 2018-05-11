0e0a2cdc37b50a697ad74452213803349008b662
InFeRnO_w <couvreux_corentin@yahoo.fr>
2018-05-11 14:34:18 +0200

    Added bootstrap, finished css basis

--- Alphabetic-order.php
+++ Alphabetic-order.php
@@ -13,6 +13,9 @@
 		<title>Manga-Animes</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
--- INCLUDES/postInsert-inc.php
+++ INCLUDES/postInsert-inc.php
@@ -4,9 +4,13 @@ session_start();
     $sql = "SELECT * FROM posts ORDER BY p_id DESC";
     $result = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_assoc($result)) {
-         echo "<div id='postBox' style='border: 1px solid rgba(0,0,0,0.5); border-radius: 15px; box-shadow: 2px 2px 2px black; background: rgba(0,0,0,0.65)'>
-             <div class='titleBox' style='float: left;margin-left: 2%;padding: 0px 7.5px 7.5px 5px;>
-                <h3 style='color: white;'>" .$row['p_title']. "</h3>
-                <h4>Status: " .$row['p_status']. "</h4>
-                <h4>Type: " .$row['p_type']. "</h4>";
+         echo "<div class='container-fluid' style='padding-top: 50px;'>
+         <div class='row'>
+         <div id='postBox' style='border: 1px solid rgba(0,0,0,0.5); border-radius: 15px; box-shadow: 2px 2px 2px black; background: rgba(0,0,0,0.65); width: 100%;'>
+                <div class='col-lg-4'><h3 style='color: white;'>" .$row['p_title']. "</h3></div>
+                <div class='col-lg-4'><h4 style='color: white;'>Status: " .$row['p_status']. "</h4></div>
+                <div class='col-lg-4'><h4 style='color: white;'>Type: " .$row['p_type']. "</h4></div>
+                </div>
+                </div>
+                </div>";
     }
