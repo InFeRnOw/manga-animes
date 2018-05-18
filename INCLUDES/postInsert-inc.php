fc37c495ad222327fe5ec97c39d99fef8013f911
InFeRnO_w <couvreux_corentin@yahoo.fr>
2018-05-18 12:24:21 +0200

    Started linking posts

--- INCLUDES/postInsert-inc.php
+++ INCLUDES/postInsert-inc.php
@@ -7,7 +7,7 @@ session_start();
          echo "<div class='container-fluid' style='padding-top: 50px;'>
          <div class='row'>
          <div id='postBox' style='border: 1px solid rgba(0,0,0,0.5); border-radius: 15px; box-shadow: 2px 2px 2px black; background: rgba(0,0,0,0.65); width: 100%;'>
-                <div class='col-lg-4'><h3 style='color: white;'>" .$row['p_title']. "</h3></div>
+                <div class='col-lg-4'><h3 style='color: white;'><a href='$row[p_link]'>" .$row['p_title']. "</a></h3></div>
                 <div class='col-lg-4'><h4 style='color: white;'>Status: " .$row['p_status']. "</h4></div>
                 <div class='col-lg-4'><h4 style='color: white;'>Type: " .$row['p_type']. "</h4></div>
                 </div>
