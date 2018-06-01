dbc1b6dae65b03d215fe0d09be7958b063dd2a20
InFeRnO_w <couvreux_corentin@yahoo.fr>
2018-06-01 22:11:35 +0200

    small fix to NewsPost.php

--- NewsPost.php
+++ NewsPost.php
@@ -1,6 +1,11 @@
 <?php
     session_start();
     include_once 'INCLUDES/dbh-inc.php';
+
+    $sql = "SELECT * FROM news WHERE n_id = 1";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $content = $row['n_content'];
 ?>
 <!DOCTYPE HTML>
 <!--
@@ -217,7 +222,7 @@ function transformDivData()// This runs the code once your browser has finished 
                    <img class="intLink" title="Paste" onclick="formatDoc('paste');" src="data:image/gif;base64,R0lGODlhFgAWAIQUAD04KTRLY2tXQF9vj414WZWIbXmOrpqbmpGjudClFaezxsa0cb/I1+3YitHa7PrkIPHvbuPs+/fvrvv8/f///////////////////////////////////////////////yH5BAEAAB8ALAAAAAAWABYAAAWN4CeOZGmeaKqubGsusPvBSyFJjVDs6nJLB0khR4AkBCmfsCGBQAoCwjF5gwquVykSFbwZE+AwIBV0GhFog2EwIDchjwRiQo9E2Fx4XD5R+B0DDAEnBXBhBhN2DgwDAQFjJYVhCQYRfgoIDGiQJAWTCQMRiwwMfgicnVcAAAMOaK+bLAOrtLUyt7i5uiUhADs=" />
                </div>
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                  <div id="textBox" contenteditable="true"></div>
+                  <div id="textBox" contenteditable="true"><?php echo $content;?>></div>
                   <input id=formContent type="hidden" name="content" />
                 </div>
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
