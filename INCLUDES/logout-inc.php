840aba035a1cb5abd7560895cf1f4d6d0ea6610a
InFeRnOw <couvreux_corentin@yahoo.fr>
2018-06-14 22:41:38 +0200

    Changing to new version

--- INCLUDES/logout-inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_POST['submit'])) {
-    session_start(); //Start the current session
-    session_unset();
-    session_destroy(); //Destroy it! So we are logged out now
-    header("Location: ../index.php?logout=succes");
-    exit();
-}
