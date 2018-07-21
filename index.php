6c52001fd1aff261898c2d6eae2fe4c7bf7c4cea
InFeRnO_w <couvreux_corentin@yahoo.fr>
2018-07-21 21:49:03 +0200

    Index banner mobile size changed

--- index.php
+++ index.php
@@ -35,8 +35,8 @@ if (!isset($_SESSION['CREATED'])) {
 	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-		<link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
-			<script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css" rel="stylesheet">
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
 		<link rel="stylesheet" href="CSS/main.css">
 	</head>
 	<body>
