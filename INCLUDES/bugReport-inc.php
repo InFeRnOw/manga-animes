c9eea835570cbfac72095df5905ee0dbec524d2e
Loki943 <36300755+Loki943@users.noreply.github.com>
2018-07-31 23:44:39 +0200

    Bug report system working

---
+++ INCLUDES/bugReport-inc.php
@@ -0,0 +1,21 @@
+<?php 
+session_start();
+include 'dbh-inc.php';
+
+if (isset($_POST['submit'])) {
+    $name = $_SESSION['u_uid'];
+    $email = $_SESSION['u_email'];
+    $subject = mysqli_real_escape_string($conn, $_POST['bugSubject']);
+    $text = mysqli_real_escape_string($conn, $_POST['bugText']);
+    
+    if (empty($text)) {
+        header("Location: ../bug-report.php?report=empty");
+        exit();
+    }
+    else {
+        $sql = "INSERT INTO bugreport (bugr_author, bugr_email, bugr_subject, bugr_text) VALUES ('$name', '$email', '$subject', '$text');";
+        $result = mysqli_query($conn, $sql);
+        header("Location: ../bug-report.php?report=sucess");      
+    }
+}
+
