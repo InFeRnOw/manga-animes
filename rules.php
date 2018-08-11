6e92c0005f8cdf24a9312b506ac1e25f1c5fca47
InFeRnO_w <couvreux_corentin@yahoo.fr>
2018-08-11 04:33:15 +0200

    added rules.php and changed h1 to h2 in aboutus.php

--- rules.php
+++ rules.php
@@ -47,9 +47,25 @@ if (!isset($_SESSION['CREATED'])) {
 
 			<div id="page">
 				<section id="post">
-					<div class="divider-with-content"><h1>About us</h1></div>
-						<h2>Who are we?</h2>
-	          <p>Hi, we are two french highschool students whom for a class project wanted to create a website about Manga and Animes!</p>
+					<div class="divider-with-content"><h1>Our rules</h1></div>
+  					<h2>1. Be friendly</h2>
+            <p>Give advices, help others, try filling up your description and putting a profile picture.</p>
+            <h2>2. Be respectful</h2>
+            <p>Everyone makes mistakes, everyone has his own way of doing a post (will be corrected by admins/modos).</p>
+            <h2>3. Copyrights</h2>
+            <p>If your reference isn't your own personal referencement, please credit the real author (use a link if possible or a name/username).</p>
+            <p>Please give credits to the author of images you use for the respect of his work and please understand that if an author of any image asks to take away one image, we will do so and ask for you to find a replacement.</p>
+            <h2>4. Images posted by users</h2>
+            <p>Do not post any innapropriate images, neither on a post nor on your profile picture.</p>
+            <h2>5. Help the website</h2>
+            <p>Please report any bugs encountered, so that we may fix as quick as possible.</p>
+            <p>If you wanna participate in it's developpement please contact us (manga.animes.website@gmail.com).</p>
+            <h2>6. Contact</h2>
+            <p>Please do not abuse of the contact system, once sended it may take a while for an admin to see it (maximum 72H).</p>
+            <h2>7. Multiple accounts</h2>
+            <p>Please do not make double accounts, if you lost yours then you may do so but for contacting us (normally you should be able to get it back with the "forget username or password ?").</p>
+            <h2>7. Suggestions</h2>
+            <p>For suggestions, go to the contact section then to suggestions, please do not make absurd suggestions.</p>
 					</section>
 				</div>
 
