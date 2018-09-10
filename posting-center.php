8f68b771489b08dfc6a2ac6eabb7eb5dac825f6b
Loki943 <36300755+Loki943@users.noreply.github.com>
2018-09-10 15:45:54 +0200

    Js Post Center with some bugs

--- posting-center.php
+++ posting-center.php
@@ -18,6 +18,7 @@ $genre = $_SESSION['SaveTemp_genre'];
 $bannerCreator = $_SESSION['SaveTemp_Creator'];
 $bannerCreatorPageLink = $_SESSION['SaveTemp_CreatorPage'];
 $imgLink = $row['p_img_src'];
+$numberArcsCenter = $_SESSION['SaveTemp_ArcsCenter'];
 
 switch ($status) {
     case 'In progress':
@@ -283,6 +284,9 @@ if (!isset($_SESSION['CREATED'])) {
                   <div class="col-md-6 col-xs-12 marginForm">
                      <input type="text" name="bannerCreatorPageLink" value="<?php echo $bannerCreatorPageLink ?>" placeholder="Work page of artist">
                   </div>
+                <div class="col-md-6 col-xs-12 marginForm">
+                     <input id="arcsCenter" type="number" name="arcNumbersCenter" value="<?php echo $numberArcsCenter ?>" placeholder="Total Arcs">
+                  </div>
                             <!-- Change to a list with seasons -->
                   <div class="row">
                     <div class="col-xs-12">
@@ -292,8 +296,23 @@ if (!isset($_SESSION['CREATED'])) {
                                   $firstFix = stripslashes($content);
                                   $SecondFix = str_replace("rn","",$firstFix);
                                   echo $SecondFix;
-                      				}
-  							?>
+                      				} 
+                                else {
+                  								
+                                  echo '<table class="table arcTable table-bordered">
+                                          <tbody>
+                                            <tr id="RowFirst">
+                                              <td>
+                                                <h3><b style="background-color: rgb(255, 255, 255);">Animes Arcs</b></h3>
+                                              </td>
+                                              <td>
+                                              <h3><b style="background-color: rgb(255, 255, 255);">Arc - Chapters</b></h3>
+                                              </td>
+                                            </tr>
+                                          </tbody>
+                                        </table>';
+        							          }
+  							           ?>
                       </textarea>
                     </div>
                   </div>
