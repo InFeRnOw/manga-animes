be3f034db8d983fea37b6de32c437637050ae4a4
Loki943 <36300755+Loki943@users.noreply.github.com>
2018-10-13 15:35:10 +0200

    Tutorial edit post added // bug when editing

---
+++ tutorialPost.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+include_once 'INCLUDES/dbh-inc.php';
+
+if (!isset($_SESSION['u_id'])) {
+    header("Location: ../login.php");
+}
+else {
+    $sql = "SELECT * FROM tutorial WHERE t_id = 1";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+
+    $content = $row['t_content'];
+}
+
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+	// last request was more than 30 minutes ago
+	session_unset();     // unset $_SESSION variable for the run-time
+	session_destroy();   // destroy session data in storage
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+
+if (!isset($_SESSION['CREATED'])) {
+    $_SESSION['CREATED'] = time();
+} else if (time() - $_SESSION['CREATED'] > 1800) {
+    // session started more than 30 minutes ago
+    session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
+    $_SESSION['CREATED'] = time();  // update creation time
+}
+?>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Manga-Animes</title>
+  		<meta charset="utf-8" />
+  		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link href="https://fonts.googleapis.com/css?family=Roboto+Slab" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/css/bootstrap-select.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.9/summernote.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.4/js/bootstrap-select.min.js"></script>
+		<link rel="stylesheet" href="CSS/main.css">
+	</head>
+	<body>
+    <div class="container-fluid">
+			<!-- Header -->
+				<div id="header">
+
+					<?php include 'INCLUDES/html-inc/htmlHeaderMain-inc.php' ?>
+
+          <div class="divider-nav"></div>
+
+				</div>
+
+        <div id="page">
+          <section id="header">
+            <h2>Edit Tutorial</h2>
+          </section>
+
+          <?php include 'INCLUDES/errors-inc.php'?>
+
+					<section id="post">
+						<div class="container-fluid">
+              <form action="INCLUDES/tutorialEdit-inc.php" method="POST">
+                  <div class="row">
+                    <div class="col-xs-12">
+                      <h4><u>Tutorial</u></h4>
+                      <textarea id="summernote" name="content"><?php echo $content ?></textarea>
+                    </div>
+                  </div>
+                    <button class="btn button-gray" type="submit" name="submit">Edit Tutorial</button>
+                  </form>
+  						</div>
+					</section>
+				</div>
+
+			<!-- Footer -->
+				<div id="footer">
+
+          <?php include 'INCLUDES/html-inc/htmlFooterMain-inc.php' ?>
+
+				</div>
+      </div>
+	</body>
+</html>
